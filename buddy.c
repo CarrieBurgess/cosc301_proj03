13d97dc99a5f3054f232d58e195d877339d73ca5
cosc301 <cosc301@csvm.(none)>
2013-10-23 18:23:39 -0400

    Finished dump_memory_map, added a few other things

--- buddy.c
+++ buddy.c
@@ -1,7 +1,8 @@
 /*
 
 Title: Project 03
-Purpose: to recreate malloc and free utilizing the buddy system
+Purpose: to recreate a method to malloc goodness and find a way to let free the evils of the world
+(make a malloc and free)
 Masterminds: Shreeya Khadka, the Robot from Pluto, and Carrie Burgess, an evil world ruler
 Date of world domination (when due): 4 November 2013
 
@@ -61,7 +62,7 @@ void *malloc(size_t request_size) {
 		
         atexit(dump_memory_map);
         write_header(heap_begin, (1024*1024), 0);
-        first_free = heap_begin;
+        first_free = heap_begin; //this points to first free chunk, BEFORE header
     }
     request_size = request_size+8; //add 8 bits
     int i = 0;
@@ -72,16 +73,42 @@ void *malloc(size_t request_size) {
     } 
     request_size = pow;
 	printf("size %d\n",(int) request_size);
-   
+	
+	
+   return NULL; //CHANGE THIS!!  Need to return pointer to allocated space (after 8 bits)
 }
 
 void free(void *memory_block) {
 	
 }
 
+
+
 void dump_memory_map(void) {
+	printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
 	int size;
-	int offset;
+	int offset = 0; //difference between chunks
+	void * pointer = first_free;
+	if(pointer != heap_begin) { //deals with if malloced memory at beginning
+		int size = (char*)first_free - (char*)heap_begin;
+		printf("Block size: %d bits.  Segment is allocated.\n", size);
+	}
 	read_header(pointer, &size, &offset);
+	printf("Block size: %d bits.  Offset: %d bits.  Segment is free.\n", size, offset);
+	while(offset != 0) {
+		if(offset>size) { //for segments allocated b/w free chunks
+			size = offset-size;
+			printf("Block size: %d bits.  Segment is allocated.\n", size);
+		}
+		pointer = pointer + offset; //units should work...
+		read_header(pointer, &size, &offset);	
+		printf("Block size: %d bits.  Offset: %d bits.  Segment is free.\n", size, offset);
+	}
+	if(offset>size) { //if there is one last allocated chunk after last free chunk
+		size = offset-size;
+		printf("Block size: %d bits.  Segment is allocated.\n", size);
+	}
+	printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");	
 }
+
 
